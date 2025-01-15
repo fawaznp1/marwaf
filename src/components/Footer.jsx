4effebed7df30edd6e9314ea0fe50b6209b2e840
fawaz <fawaznp3598@gmail.com>
2025-01-15 16:38:26 +0530

    yearupdate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -101,36 +101,9 @@ export default function App() {
       </MDBContainer>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        <p>
-          &copy; 2024 @ marwaarts.in. All Rights Reserved.
-        </p>
-       {/*  <Button variant="primary" onClick={() => setShow(true)}>
-        Custom Width Modal
-      </Button>
-
-      <Modal
-        show={show}
-        onHide={() => setShow(false)}
-        dialogClassName="modal-90w"
-        aria-labelledby="example-custom-modal-styling-title"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="example-custom-modal-styling-title">
-            Custom Modal Styling
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
           <p>
-            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-            ipsam atque a dolores quisquam quisquam adipisci possimus
-            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-            deleniti rem!
+             &copy; {new Date().getFullYear()} @ marwaarts.in. All Rights Reserved.
           </p>
-        </Modal.Body>
-      </Modal> */}
           <a href="/privacynpolicy"  className="text-white me-2">Privacy Policy</a> 
           <a href="/terms" className="text-white ms-2">Terms of Service</a>
           <p className='mt-4'>Created by fawaz_np <a href="https://fawaznp.vercel.app/" className='text-white' target='_blank'><i className="fa-solid fa-up-right-from-square"></i></a></p> 
